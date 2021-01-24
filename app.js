3730a5049d8d73c59b25075feac106398e74d69f
masutaniR <popohu87@gmail.com>
2021-01-24 15:13:26 +0900

    [Start] JS-6

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
 $(function(){
-  $('.box1').slideDown(5000, function(){
-    $('.box1').css({
-      'background-color' : '#0000ff',
-      'height' : '100px'
-    }).slideUp(5000);
+  $('.box1').mouseover(function(){
+    $('.box1').css({'background-color' : '#0000ff'});
+  });
+  $('.box1').mouseout(function(){
+    $('.box1').css({'background-color' : '#ff0000'});
   });
 });
